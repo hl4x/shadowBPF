83d14cfcbc3d673399e9b963a574e83b90eb72bb
hl4x <hl4x@proton.me>
2024-09-20 18:40:38 +0000

    first commit

---
+++ src/shadow.bpf.c
@@ -0,0 +1,15 @@
+#include "vmlinux.h"
+#include <bpf/tracing.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("kprobe/unix_stream_sendmsg")
+BPF_KPROBE(unix_stream_sendmsg, struct socket *sock, struct msghdr *msg,
+			       size_t len)
+{
+    int pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_printk("KPROBE ENTRY pid = %d\n", pid);
+    return 0;
+}
---
+++ src/shadow.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stderr.h>
+#include <signal.h>
+#include <bpf/libbpf.h>
+#include "shadow.skel.h"
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list *args)
+{
+    return vfprintf(stderr, format, args);
+}
+
+static volatile sig_atomic_t exiting = 0;
+
+static void sig_handler(int signo)
+{
+    exiting = 1;
+}
+
+int main(int argc, char **argv)
+{
+    struct shadow_bpf *skel; // double check when skeleton file is created
+    int err;
+
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
+
+    /* cleaner handling of ctrl+c */
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);1
+
+    /* load and verify BPF programs */
+    skel = kprobe_bpf__open_and_load();
+    if (!skel) {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        goto cleanup;
+    }
+
+    /* attach kprobes */
+    err = shadow_bpf__attach(skel);
+    if (err) {
+        fprint(stderr, "Failed to attach BPF skeleton\n")
+        goto cleanup;
+    }
+
+    printf("Successfully started!\n"); 
+
+    while (!exiting) {
+        sleep(1);
+    }
+
+    cleanup:
+        shadow_bpf__destroy(skel);
+        return -err;
+}

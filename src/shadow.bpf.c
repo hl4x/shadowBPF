509267247b0228e514164be0db7350487c738fef
hl4x <hl4x@proton.me>
2024-09-22 04:15:48 +0000

    read data from msghdr

--- src/shadow.bpf.c
+++ src/shadow.bpf.c
@@ -7,17 +7,20 @@
 #define SOCK_PATH_OFFSET    \
     (offsetof(struct unix_address, name) + offsetof(struct sockaddr_un, sun_path))
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char _license[] SEC("license") = "GPL";
 
-static inline int cmp_sun_path(const char *sun_path)
+struct iov_data {
+    size_t len;
+    void *base;
+};
+
+static int cmp_sun_path(const char *sun_path)
 {
     static const char path[] = "/run/docker.sock";
-
-    if (bpf_strncmp(sun_path, sizeof(path), &path)) { 
+    if (!bpf_strncmp(sun_path, sizeof(path), &path)) { 
         /* match */
         return 1;
     }
-
     return 0;
 }
 
@@ -26,16 +29,22 @@ int BPF_KPROBE(unix_stream_sendmsg, struct socket *sock, struct msghdr *msg,
 			       size_t len)
 {
     char sun_path[UNIX_PATH_MAX] = { 0 };
-    
+
     /* read sun_path */
     struct unix_sock *us = (struct unix_sock *)BPF_CORE_READ(sock, sk);
     char *addr = (char *)BPF_CORE_READ(us, addr);
     bpf_probe_read_kernel(&sun_path, UNIX_PATH_MAX, addr + SOCK_PATH_OFFSET); 
 
     /* check if sun_path is /run/docker.sock */    
-    if (cmp_sun_path(&sun_path)) {
-        bpf_printk("Matched /run/docker.sock: %s", &sun_path);
-    }
-    
+    if (!cmp_sun_path(&sun_path))
+        return 0;
+    bpf_printk("Matched: %s", &sun_path);
+
+    /* docker seems to only use ITER_UBUF so just read __ubuf_iovec */
+    struct iov_data data;
+    data.len = BPF_CORE_READ(msg, msg_iter.__ubuf_iovec.iov_len);
+    data.base = BPF_CORE_READ(msg, msg_iter.__ubuf_iovec.iov_base);
+    bpf_printk("%s %d", data.base, data.len);
+
     return 0;
 }

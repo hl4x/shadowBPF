4f0300ec1e556d7fa0450b4cacb0f24df5a2c162
hl4x <hl4x@proton.me>
2025-02-05 17:14:33 +0000

    clean up

--- src/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/vagrant/shadowBPF/src/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/vagrant/shadowBPF/bpftool/include/linux/compiler.h \
- /home/vagrant/shadowBPF/bpftool/include/linux/compiler_types.h \
- /home/vagrant/shadowBPF/bpftool/include/linux/compiler-gcc.h

